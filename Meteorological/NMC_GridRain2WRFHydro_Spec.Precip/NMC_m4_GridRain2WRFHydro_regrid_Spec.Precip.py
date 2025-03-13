fix [units] error while regrid

Meteorological/NMC_GridRain2WRFHydro_Spec.Precip/NMC_m4_GridRain2WRFHydro_regrid_Spec.Precip.py
@@ -38,9 +38,9 @@ def valuecheck(m4_value):
     sorted_m4_value = np.sort(m4_value, axis=None)[::-1]
     print(f"Sorted m4_value: {sorted_m4_value[0:15]}")
 
-def process_m4_file(i, m4_info):
-    m4_name = m4_info[0][i]
-    time = m4_info[1][i]
+def process_m4_file(m4_info):
+    m4_name = m4_info[0]
+    time = m4_info[1]
     m4_path = os.path.join(m4path, m4_name)
     m4_value = readm4(m4_path)
     m4_ds = xr.Dataset(
@@ -61,7 +61,7 @@ def process_m4_file(i, m4_info):
 def precip_regrid(forcing, geo_em, geo_sm):
 
     regridder = xe.Regridder(forcing, geo_em, 'bilinear')
-    forcing_regrid = regridder(forcing.precip)
+    forcing_regrid = regridder(forcing.precip_rate)
     forcing_regrid.coords['west_east'] = geo_sm.x.values
     forcing_regrid.coords['south_north'] = geo_sm.y.values
     forcing_regrid = forcing_regrid.rename({'west_east': 'x', 'south_north': 'y'})
@@ -71,15 +71,19 @@ def precip_regrid(forcing, geo_em, geo_sm):
     return forcing_regrid
 
 def save_PRECIP_FORCING(forcing_regrid):
+    global out_path
     dates = pd.to_datetime(forcing_regrid.time.values)
     for i in range(dates.size):
         str = dates[i].strftime('%Y%m%d%H')
         precip_forcing = forcing_regrid.isel(time=[i]).to_dataset(name="precip_rate")
-        precip_forcing.to_netcdf(f"./output_files/{str}00.PRECIP_FORCING.nc")
+        precip_forcing.attrs['description'] = "China_Hourly_Merged_Precipitation_Analysis(CHMPA)_Data"
+        precip_forcing.attrs['units'] = "mm/s"
+        precip_forcing.to_netcdf(f"{out_path}/{str}00.PRECIP_FORCING.nc")
 
 start = datetime(2023, 7, 1, 0)
 end = datetime(2023, 9, 15, 23)
-m4path = r"./2023"
+m4path = "./2023"
+out_path = "./output_files"
 lon_min = 111.7
 lon_max = 120.0
 lat_min = 35.0
@@ -87,8 +91,9 @@ lat_max = 43.0
 grid_resolution = 0.01
 lon = (np.arange(lon_min*100, (lon_max + grid_resolution)*100, grid_resolution*100)) / 100
 lat = (np.arange(lat_min*100, (lat_max + grid_resolution)*100, grid_resolution*100)) / 100
-m4_info = m4name(start, end)
-time_coords = pd.date_range(start, end, freq="h")
+m4_name = m4name(start, end)
+time_coords = pd.date_range(start, end, freq="h", tz="Asia/Shanghai").tz_convert("UTC").tz_localize(None)
+m4_info = np.array(list(zip(m4_name, time_coords)))
 
 geo_sm = "./GEOGRID_LDASOUT_Spatial_Metadata.nc"
 geo_em = "./geo_em.d03.nc"
@@ -101,24 +106,19 @@ geo_em_ds['lon'] = geo_em_ds['lon'].sel(Time=0, drop=True)
 
 npart = (len(m4_info) // 720) + 1
 div_m4_info = np.array_split(m4_info, npart)
-div_time_coords = np.array_split(time_coords, npart)
 
 print("Total number of parts: ", len(div_m4_info))
 for i in range(len(div_m4_info)):
-    print("Processing part: ", i)
+    print("Processing part: ", i+1)
+    
     datasets = []
-    part_len = len(div_m4_info[i])
-    div_m4_info = [div_m4_info[i], div_time_coords[i]]
-
-    results = Parallel(n_jobs=-1)(delayed(process_m4_file)(index, div_m4_info) for index in range(part_len))
+    results = Parallel(n_jobs=-1)(delayed(process_m4_file)(m4) for m4 in div_m4_info[i])
 
     datasets.extend(results)
     forcing_ds = xr.concat(datasets, dim="time")
 
     forcing_ds['precip_rate'] = forcing_ds.precip / (60.0*60.0)
+    forcing_ds = forcing_ds.drop_vars('precip')
     forcing_regrid = precip_regrid(forcing_ds, geo_em_ds, geo_sm_ds)
     save_PRECIP_FORCING(forcing_regrid)
-
-
-
 